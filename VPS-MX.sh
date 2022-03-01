4a184cbc88c987c3d71924ed3d21a79261ae0e58
diesel09 <37728951+diesel09@users.noreply.github.com>
2022-03-01 10:21:59 -0500

    Update VPS-MX.sh

--- VPS-MX.sh
+++ VPS-MX.sh
@@ -180,9 +180,9 @@ MIP2=$(wget -qO- ipv4.icanhazip.com)
 [[ "$MIP" != "$MIP2" ]] && IP="$MIP2" || IP="$MIP"
 }  
 function_verify () {
-  permited=$(curl -sSL "https://raw.githubusercontent.com/VPS-MX/VPS-MX-8.0/master/Control-IP")
+  permited=$(curl -sSL "https://raw.githubusercontent.com/diesel09/8.0/master/Control-IP")
   [[ $(echo $permited|grep "${IP}") = "" ]] && {
-  echo -e "\n\n\n\033[1;95m======================================================\n ¡ESTA KEY NO CONCUERDA CON EL INSTALADOR!,CONATACTE A @Kalix1\n======================================================\n"
+  echo -e "\n\n\n\033[1;95m======================================================\n ¡ESTA KEY NO CONCUERDA CON EL INSTALADOR!,CONATACTE A @FelipeCouoh\n======================================================\n"
   [[ -d /etc/newadm ]] && rm -rf /etc/newadm
   exit 1
   } || {
